8c55d121b53c634d388208bf864cf4b030064e03
Benjamin <owolabenjade@gmail.com>
2024-02-26 09:28:42 -0800

    duro

--- shell.h
+++ shell.h
@@ -1,6 +1,20 @@
-#ifndef HEADER_FILE
-#define HEADER_FILE
+#ifndef SHELL_H
+#define SHELL_H
+
+/* Standard Library Includes for input/output, memory allocation, and process control */
+#include <stdio.h>  /* For printf and getline functions */
+#include <stdlib.h> /* For exit, malloc, and free functions */
+#include <unistd.h> /* For fork, getpid, execve functions */
+#include <string.h> /* For string manipulation functions, if needed */
+#include <sys/types.h> /* For pid_t type */
+#include <sys/wait.h> /* For wait function */
+
+/* Add any custom function prototypes here.
+   For the current implementation in simple_shell.c, we don't have custom functions.
+   However, if you expand your shell, you might add function prototypes like:
+void my_function(void);
+*/
 
 int main(void)
 
-#endif /* HEADER_FILE */
+#endif /* SHELL_H */
